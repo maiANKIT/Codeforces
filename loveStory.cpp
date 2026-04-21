f47a5e00e9515b234e205f337fc5426c5056b368
maiANKIT <ankitsharmaindiaa@gmail.com>
2026-04-21 19:58:12 +0530

    done

---
+++ loveStory.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        string s;
+        cin>>s;
+
+        string x = "codeforces";
+
+        int count = 0;
+
+        for(int i = 0; i<10; i++){
+            if(x[i] != s[i]) count++;
+        }
+
+        cout<<count<<endl;
+
+    }
+
+   return 0;
+}
