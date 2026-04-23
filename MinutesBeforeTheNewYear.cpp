6cd1d74b2d83bf782991a3bf89dcb3c62a3ba092
maiANKIT <ankitsharmaindiaa@gmail.com>
2026-04-23 20:09:37 +0530

    done

---
+++ MinutesBeforeTheNewYear.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        int h, m;
+        cin>>h>>m;
+
+        int x = (23 - h) * 60;
+
+        cout<<60 - m+x<<endl;
+        
+
+    }
+
+
+   return 0;
+}
