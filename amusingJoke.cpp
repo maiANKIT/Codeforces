b1ff60f55ef39b505eba8c9a656ccbcca3cab39d
maiANKIT <ankitsharmaindiaa@gmail.com>
2026-05-18 23:44:00 +0530

    done

---
+++ amusingJoke.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    string a, b, c;
+    cin>>a>>b>>c;
+
+    string d = a + b;
+
+    sort(d.begin(), d.end());
+    sort(c.begin(), c.end());
+    if(d == c) cout<<"YES";
+    else cout<<"NO";
+
+   return 0;
+}
