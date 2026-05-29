6c0d6ba4cfc358df881cdec2da2af469db218b7b
maiANKIT <ankitsharmaindiaa@gmail.com>
2026-05-29 15:18:17 +0530

    done

--- 2-3moves.cpp
+++ 2-3moves.cpp
@@ -14,48 +14,13 @@ int main()
         int n;
         cin >> n;
 
-        if (n % 3 == 0)
-        {
-
+        if(n == 1) cout<<2<<endl;
+        else if (n % 3 == 0)
             cout << n / 3 << endl;
-        }
         else
         {
-
-            int count1 = 0;
-
-            int a = n;
-
-            if (n == 1)
-                cout << 2 << endl;
-            else
-            {
-
-                while (n > 0)
-                {
-
-                    n = n - 3;
-                    count1++;
-                }
-
-                if (n == -1)
-                    count1 = count1 + 1;
-                else if (n == -2)
-                    count1 = count1 + 2;
-
-                int count2 = 0;
-                while (a > 0)
-                {
-
-                    a = a - 2;
-                    count2++;
-                }
-
-                if (a == -1)
-                    count2--;
-
-                cout << min(count1, count2) << endl;
-            }
+            int x = n + (3 - n % 3);
+            cout << x / 3 << endl;
         }
     }
 
