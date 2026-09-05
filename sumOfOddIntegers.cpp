498002d5c4c5ee1d832cbf25bf6b9e9a32fd6f52
maiANKIT <ankitsharmaindiaa@gmail.com>
2026-09-05 23:53:28 +0530

    done

---
+++ sumOfOddIntegers.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        if (n % k == 0)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
