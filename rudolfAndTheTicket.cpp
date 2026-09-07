ce7d87f5c2c6bac568967efba26206bcfa12cb84
maiANKIT <ankitsharmaindiaa@gmail.com>
2026-09-07 22:47:14 +0530

    done

---
+++ rudolfAndTheTicket.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        int n, m, k;
+        cin>>n>>m>>k;
+
+        vector<int> a(n), b(m);
+        for(int i = 0; i<n; i++) cin>>a[i];
+        for(int i = 0; i<m; i++) cin>>b[i];
+
+        int count = 0;
+        for(int i = 0; i<n; i++){
+
+            for(int j = 0; j<m; j++){
+                if(a[i] + b[j] <= k) count++;
+            }
+
+        }
+
+        cout<<count<<endl;
+
+    }
+
+   return 0;
+}
